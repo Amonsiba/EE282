714f5c29a59840978e98fad60dc5057bb5d3b780
Amonsiba <amonsiba@uci.edu>
2020-11-05 22:07:15 -0800

    Updated code for this section of homework3

--- code/scripts/hw3_genome_summary.sh
+++ code/scripts/hw3_genome_summary.sh
@@ -1,12 +1,11 @@
-
 #!/usr/bin/env bash
+
+#Download file 
+wget ftp://ftp.flybase.net/genomes/Drosophila_melanogaster/dmel_r6.36_FB2020_05/fasta/dmel-all-chromosome-r6.36.fasta.gz
 
 #File Integrity 
 wget ftp://ftp.flybase.net/genomes/Drosophila_melanogaster/dmel_r6.36_FB2020_05/fasta/md5sum.txt
 md5sum --check <(grep dmel-all-chromosome-r6.36.fasta.gz md5sum.txt)
-
-
-
 
 #Calculate Summaries of the Genome 
 #1. Total nuber of nucleotides - 142573024
