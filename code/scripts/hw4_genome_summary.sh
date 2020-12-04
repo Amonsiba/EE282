2313b839d65eec911c61bf77c13dc3eb739d02bd
amonsiba <amonsiba@uci.edu>
2020-12-04 11:59:02 -0800

    added shebang and downloading file

---
+++ code/scripts/hw4_genome_summary.sh
@@ -0,0 +1,8 @@
+#!/user/bin/env bash
+
+#Summarize Genome Assembly 
+
+#Downloading file from the internet - chromosome file 
+wget ftp://ftp.flybase.net/releases/current/dmel_r6.36/fasta/dmel-all-chromosome-r6.36.fasta.gz
+
+
