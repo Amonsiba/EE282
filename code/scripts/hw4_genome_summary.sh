46cecb73fa56f1560c949c3498f2dc6c00f7b265
amonsiba <amonsiba@uci.edu>
2020-12-04 17:26:37 -0800

    updated code for gzip files

--- code/scripts/hw4_genome_summary.sh
+++ code/scripts/hw4_genome_summary.sh
@@ -12,20 +12,20 @@ md5sum --check <(grep dmel-all-chromosome-r6.36.fasta.gz md5sum.txt)
 #Calculate the following for all sequences <= 100 kb and all sequences >100 kb: 
 
 #Code
-#Unzipping the gz file
-gunzip dmel-all-chromosomes-r6.36.fasta.gz
 
-#Make the files for specific length of the sequence
+#Makes the files for specific length of the sequence
 #For sequences >100 kb:
-bioawk -c fastx 'length($seq) > 100000{ print ">"$name; print $seq }'  dmel-all-chromosome-r6.36.fasta > greaterthan.txt
+bioawk -c fastx 'length($seq) > 100000{ print ">"$name; print $seq }'  dmel-all-chromosome-r6.36.fasta.gz | gzip -c > dmelr6.gt.fa.gz
+
 #For sequences <=100 kb:
-bioawk -c fastx 'length($seq) <= 100000{ print ">"$name; print $seq }'  dmel-all-chromosome-r6.36.fasta > lessorequal.txt
+bioawk -c fastx 'length($seq) <= 100000{ print ">"$name; print $seq }'  dmel-all-chromosome-r6.36.fasta.gz | gzip -c > dmelr6.lte.fa.gz
+
 
 #Identifying Nucleotide, Ns and sequences
 #For the sequence >100 kb: 
-faSize greaterthan.txt
+faSize dmelr6.gt.fa.gz
 #For the sequence <=100 kb:
-faSize lessorequal.txt
+faSize dmelr6.lte.fa.gz
 
 #Answers
 #For the sequences >100 kb"
@@ -38,9 +38,17 @@ faSize lessorequal.txt
 #2. Total number of Ns			662593
 #3. Total number of sequences		1863
 
+
 #Plots of the following for all sequences =/< 100 kb and all sequences >100 kb:
 
 #Code
+
+#Makes the file for Sequence name, sequence length and sequence GC(%)
+#For sequence > 100 kb
+bioawk -c fastx 'length($seq) > 100000{print ">"$name "\t" length($seq) "\t" gc($seq)}' dmel-all-chromosome-r6.36.fasta.gz | column -t > dmelr6.gt.txt
+#For sequences <= 100 kb:
+bioawk -c fastx 'length($seq) <= 100000{print ">"$name "\t" length($seq) "\t" gc($seq)}' dmel-all-chromosome-r6.36.fasta.gz | column -t > dmelr6.lte.txt
+
 
 #Answers 
 
