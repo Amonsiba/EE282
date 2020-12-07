9395e527a07b05c357f0de44d4c676081ff32609
amonsiba <amonsiba@uci.edu>
2020-12-07 15:16:36 -0800

    finalized code

--- code/scripts/hw4_genome_summary.sh
+++ code/scripts/hw4_genome_summary.sh
@@ -50,20 +50,60 @@ bioawk -c fastx '{ print length($seq) "\t" gc($seq) }' dmelr6.gt.fa.gz | sort  -
 #For sequences <= 100 kb:
 bioawk -c fastx '{ print length($seq) "\t" gc($seq) }' dmelr6.lte.fa.gz | sort  -k1,1rn > dmelr6.lte.txt
 
-#Answers 
-
+#Answers
+#Text Files were downloaded and process in an R script for the following questions:
 #1. Sequence length distribution (Histogram in log scale)
-
-
 #2. Sequence GC% distribution (Histogram) 
 
+#Rscript
+#Histograms for HW#4 - Bioinformatics 
+
+#Set working directory 
+setwd("C:/Users/anmon/Desktop")
+
+#Downloads the data from the working directory 
+dmelr6.lte <- read.delim("dmelr6.lte.txt", header = FALSE, col.names = c("Seq Length", "GC Percent"))
+dmelr6.gt <- read.delim("dmelr6.gt.txt", header = FALSE, col.names = c("Seq Length", "GC Percent"))
+
+#Transforming Seq Length in Log formation 
+LogSL.gt <- log(dmelr6.gt$Seq.Length)
+dmelr6.gt <- cbind(dmelr6.gt, LogSL.gt)
+LogSL.lte <- log(dmelr6.lte$Seq.Length)
+dmelr6.lte <- cbind(dmelr6.lte, LogSL.lte)
 
 
+#Histograms for Sequence Length
+hist(dmelr6.lte$LogSL.lte,
+     main = "Distribution of Seq. Length for Seq <=100kb", 
+     xlab ="Seq. Length (Log)", 
+     border = "purple", 
+     col = "darkred")
+hist(dmelr6.gt$LogSL.gt,
+     main = "Distribution of Seq. Lengthe for Seq >100kb", 
+     xlab ="Seq. Length (Log)", 
+     border = "blue", 
+     col = "darkgreen")
+
+
+#Histograms for GC Percentage
+hist(dmelr6.lte$GC.Percent,
+    main = "Histogram of GC Percentage for Seq <=100kb", 
+    xlab ="Percent Distribution", 
+    border = "purple", 
+    col = "darkred")
+hist(dmelr6.gt$GC.Percent,
+     main = "Histogram of GC Percentage for Seq >100kb", 
+     xlab ="Percent Distribution", 
+     border = "blue", 
+     col = "darkgreen")
+
+#Last question was processed in the Terminal
 #3. Cumulative sequence size sorted from the larges to smallest sequences
 #To get the cumulative sequence size plot
 #For the sequences >100 kb:
 plotCDF <(cut -f 1 dmelr6.gt.txt) /dev/stdout | display 
 #For the sequence <=100 kb:
 plotCDF <(cut -f 1 dmelr6.lte.txt) /dev/stdout | display
+
 
 
